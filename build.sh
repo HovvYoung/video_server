71f7d724ca3c79756650f36d4a76322bc2b5291a
HovvYoung <haoran199507@gmail.com>
2019-05-21 19:45:33 +0800

    .sh

--- build.sh
+++ build.sh
@@ -3,5 +3,5 @@
 # Build web UI
 cd ~/go/src/github.com/hovvyoung/video_server/web
 go install
-cp ~/go/bin/web ~/go/bin/video_server_web_ui/
-cp -R ~/go/src/github.com/hovvyoung/video_server/templates ~/go/bin/video_server_web_ui/
+cp ~/go/bin/web ~/go/bin/video_server_web_ui/web
+cp -R ~/go/src/github.com/hovvyoung/video_server/templates ~/go/bin/video_server_web_ui/web
